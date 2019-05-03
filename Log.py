e40b537a01c25d6d3f2f981437c42f4bb0ed32f0
Andrea Papaleo <apapaleo03@gmail.com>
2019-05-03 12:54:25 -0400

    Updated Log.py with new tkinter features

--- Log.py
+++ Log.py
@@ -1,20 +1,72 @@
+
+####################################################################
+
+import tkinter
+from tkinter.filedialog import askopenfilename
+from matplotlib.backends.backend_tkagg import (
+    FigureCanvasTkAgg, NavigationToolbar2Tk)
+# Implement the default Matplotlib key bindings.
+from matplotlib.backend_bases import key_press_handler
+from matplotlib.figure import Figure
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-from matplotlib.widgets import RadioButtons
-import scipy.stats as sci
-import sys
+from scipy.stats import linregress
+from matplotlib.widgets import SpanSelector
+#######################################################
+root = tkinter.Tk()
+root.wm_title("Log Analysis")
+#######################################################
+fig = Figure(figsize=(5, 4), dpi=100)
+ax = fig.add_subplot(111)
+ax.scatter([1,1,1,1,1,1,1,1,1,1.5,2,2.5,3,3,3,3,3,3,3,3,3,4,4,4,4,4,4,6],
+            [5,4.5,4,3.5,3,2.5,2,1.5,1,3,3,3,5,4.5,4,3.5,3,2.5,2,1.5,1,4,3,2.5,2,1.5,1,1])
+#######################################################
 
+canvas = FigureCanvasTkAgg(fig, master=root)  # A tk.DrawingArea.
+canvas.draw()
+canvas.get_tk_widget().grid(row=3,column=3)#,rowspan=3,columnspan=5)
+
+toolbarFrame= tkinter.Frame(master=root)
+toolbarFrame.grid(row=8,column=3,columnspan=5)
+
+toolbar = NavigationToolbar2Tk(canvas, toolbarFrame)
+toolbar.update()
+#canvas.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=1)
+
+file_loaded = False
+########################################################
+
+def on_key_press(event):
+    print("you pressed {}".format(event.key))
+    key_press_handler(event, canvas, toolbar)
+
+canvas.mpl_connect("key_press_event", on_key_press)
+
+#------------------Quit The Window------------------------#
+
+def _quit():
+    root.quit()     # stops mainloop
+    root.destroy()  # this is necessary on Windows to prevent
+                    # Fatal Python Error: PyEval_RestoreThread: NULL tstate
+
+quit_button = tkinter.Button(master=root, text="Quit", command=_quit)
+
+#-------------Read file and update param options---------------------#
+
+choices = (' ')
+var = tkinter.StringVar(root)
+var.set('')
+param_options = tkinter.OptionMenu(root,var,*choices)
+
+log_header = []
+log_df = pd.DataFrame(data={'x':[1,2,3],'y':[1,1,1]})
 
 def read_log():
-    if len(sys.argv) < 2:
-        print('** read_log.py needs the following command line arguments:\n\n'+
-                '\t\t LAMMPS log file \n \t\t LAMMPS timestep info')
-        filename = input('Enter Filename:')
-        timestep = float(input('Enter Timestep:'))
-    else:
-        filename = sys.argv[1]
-        timestep = float(sys.argv[2])
+    global log_header
+    global log_df
+    global file_loaded
+    file_loaded = True
+    filename = askopenfilename()
 
     with open(filename) as data:
             header = []
@@ -47,104 +99,72 @@ def read_log():
     log = np.asarray(log).astype(float)
 
     df_log = pd.DataFrame(data = log, columns = header)
-    df_log['Step'] = df_log['Step']*timestep
+    df_log['Step'] = df_log['Step']*0.000001
 
+    log_header = header
+    log_df = df_log
+    var.set(header[0])
+    param_options['menu'].delete(0,'end')
+    for param in header:
+        param_options['menu'].add_command(label=param, command=tkinter._setit(var,param))
 
-    fig, ax = plt.subplots()
-    ax.autoscale(enable=True,axis='both')
-    l, = ax.plot(df_log['Step'], df_log['Step'])
-    plt.subplots_adjust(left=0.3,right=0.7)
-    ave_title = fig.text(0.77,0.3,'Average Value:')
-    ave = fig.text(0.78,0.25,'')
-    slope_title = fig.text(0.77,0.2,'Slope:')
-    slope_disp = fig.text(0.78,0.15,'')
+file_button = tkinter.Button(master=root, text="Pick a File", command=read_log)
 
-
-    axcolor = 'whitesmoke'
-    ############################################################
-
-    fig.text(0.8,0.91,'X-axis',fontsize=12)
-    rax = plt.axes([0.75,0.4, 0.15,0.5], facecolor=axcolor)
-    radio_x = RadioButtons(rax,header)
-
-    def xfunc(label):
+#------------------Plot the data----------------------#
+    
+def plot():
+    global file_loaded
+    if file_loaded:
         ax.cla()
-        xdata = df_log[label]
-        ydata = df_log[radio_y.value_selected]
-        l, = ax.plot(xdata, ydata)
-        slope_disp.set_text('')
-        ax.grid(True)
-        plt.draw()
+        xdata = log_df['Step']
+        ydata = log_df[var.get()]
+        ax.plot(xdata,ydata)
+        ax.set_title(var.get()+' vs. Step')
+        ax.set_xlabel('Step')
+        ax.set_ylabel(var.get())
+        canvas.draw()
 
-    radio_x.on_clicked(xfunc)
+plot_button = tkinter.Button(root,text="Plot",command=plot)
 
-    #############################################################
-    fig.text(0.1,0.91,'Y-axis',fontsize=12)
-    rax = plt.axes([0.05, 0.4, 0.15, 0.5], facecolor=axcolor)
-    radio_y = RadioButtons(rax, header)
+#------------------------Span Selector---------------------#
+def onselect(xmin, xmax):
+    if file_loaded:
+        plot()
+        indmin, indmax = np.searchsorted(log_df['Step'], (xmin, xmax))
+        indmax = min(len(log_df['Step']) - 1, indmax)
 
-    def yfunc(label):
-        ax.cla()
-        xdata = df_log[radio_x.value_selected]
-        ydata = df_log[label]
-        l, = ax.plot(xdata, ydata)
-        ax.grid(True)
-        ave.set_text(str(round(df_log[label].mean(),3)))
-        slope_disp.set_text('')
-        plt.draw()
+        thisx = log_df['Step'][indmin:indmax]
+        thisy = log_df[var.get()][indmin:indmax]
+        slope, intercept, r, p, std = linregress(thisx,thisy)
+        print(slope)
+        ax.plot(thisx,thisx*slope+intercept)
+        #line2.set_data(thisx, thisy)
+        #ax2.set_xlim(thisx[0], thisx[-1])
+        #ax2.set_ylim(thisy.min(), thisy.max())
+        fig.canvas.draw()
 
-    radio_y.on_clicked(yfunc)
+# Set useblit=True on most backends for enhanced performance.
+span = SpanSelector(ax, onselect, 'horizontal', useblit=True,
+                    rectprops=dict(alpha=0.5, facecolor='red'))
+#-------------------Display Labels---------------------#
 
+ave_label = tkinter.Label(root,text='Average:')
+#slope_label = tkinter.Label(root,text='Slope:')
 
-    #############################################################
-    fig.text(0.1,0.31,'Hist',fontsize=12)
-    rax = plt.axes([0.05, 0.1, 0.15, 0.2], facecolor=axcolor)
-    radio_h = RadioButtons(rax, ('Pxx','Pyy','Pzz'))
+#----------------------Placements-----------------------#
 
-    def histfunc(label):
-        ax.cla()
-        ydata = df_log[label]
-        ax.hist(ydata,100)
-        ax.grid(True)
-        slope_disp.set_text('')
-        plt.draw()
+plot_button.grid(row=6,column=5)
+quit_button.grid(row=7,column=3)
+param_options.grid(row=6,column=1)
+file_button.grid(row=6,column=3)
+ave_label.grid(row=6,column=2)
+#slope_label.pack(side=tkinter.RIGHT)
 
-    radio_h.on_clicked(histfunc)
+#------------------------------------------------------#
 
-
-    while 1:
-
-        if plt.waitforbuttonpress():
-
-            pts = np.asarray(plt.ginput(2, timeout=-1))
-        
-            p1 = pts[0][0]
-            p2 = pts[1][0]
-
-
-            found_p1 = False
 
             
-            for i in range(len(df_log[radio_x.value_selected])):
-                if df_log[radio_x.value_selected][i] > p1 and not found_p1:
-                    mini = i
-                    found_p1 = True
 
-                if df_log[radio_x.value_selected][i] > p2:
-                    maxi = i  
-                    break   
-
-            slope, intercept, r, p, std = sci.linregress(df_log[radio_x.value_selected][mini:maxi],
-                                                        df_log[radio_y.value_selected][mini:maxi])
-            ph1 = ax.plot(df_log[radio_x.value_selected][mini:maxi],
-                            df_log[radio_x.value_selected][mini:maxi]*slope+intercept, 'r')
-            slope_disp.set_text(str(round(slope,3)))
-
-
-            if plt.waitforbuttonpress():
-                break
-
-            for p in ph1:
-                p.remove()
-
-    plt.show()
+tkinter.mainloop()
+# If you put root.destroy() here, it will cause an error if the window is
+# closed with the window manager.
